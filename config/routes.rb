bf8ff95c03f5487e887216e330961f77d3fd29d2
DylanGuedes <djmgguedes@gmail.com>
2015-01-30 15:00:33 -0200

    stable index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get '/about' => 'forum#about'
   get '/help' => 'forum#help'
 
+  #sections controller
+  get '/sections/:id' => 'sections#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
