d8a04132d8df06398fe6c0ba52130dddbf0f2ef6
DylanGuedes <djmgguedes@gmail.com>
2015-01-30 18:28:00 -0200

    last post logic

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,5 +7,31 @@ class Section < ActiveRecord::Base
     end
     return c
   end
+
+  def last_post
+    all_topics = self.topics
+    latest_post = ""
+    if all_topics.last
+      all_topics.each do |topic|
+        if topic.has_posts?
+          posts = topic.posts
+          posts.each do |post|
+            if !latest_post.blank?
+              if latest_post.created_at < post.created_at
+                latest_post = post
+              end
+            else
+              latest_post = post
+            end
+          end
+
+        end
+        return latest_post
+      end
+      return "no posts here! :("
+    end
+    return "No posts here! :("
+  end
+
   has_many :topics
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,11 @@
 class Topic < ActiveRecord::Base
   belongs_to :section
   has_many :posts
+  def has_posts?
+    if !self.posts.nil?
+      true
+    else
+      false
+    end
+  end
 end
