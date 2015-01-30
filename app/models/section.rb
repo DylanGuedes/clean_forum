244cd74530dd4b81023e228ef33ab46ec60b68ef
DylanGuedes <djmgguedes@gmail.com>
2015-01-30 17:40:01 -0200

    more details in forum index

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,11 @@
 class Section < ActiveRecord::Base
+  def total_posts
+    a = self.topics
+    c = 0
+    a.each do |b|
+      c += b.posts.count
+    end
+    return c
+  end
   has_many :topics
 end
