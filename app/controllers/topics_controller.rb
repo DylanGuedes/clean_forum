83fb916d7cfefc05d7727dd77d2d6d9f0bfa3d10
DylanGuedes <djmgguedes@gmail.com>
2015-01-30 15:17:10 -0200

    signup started

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
+    @posts = @topic.posts
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   #topics controller
   get '/sections/topics/:id' => 'topics#show'
 
+  #users controller
+  get '/signup' => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
