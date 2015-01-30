d9aba0aead69518fba6fdafc3415f9f39450356f
DylanGuedes <djmgguedes@gmail.com>
2015-01-30 17:07:18 -0200

    new remember token migration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(login: params[:session][:login].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      render 'new'
+    end
   end
 
   def destroy
