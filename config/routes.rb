29761eaecc1e849706a099ab829aa3a6f8c6243a
DylanGuedes <djmgguedes@gmail.com>
2015-01-30 15:12:12 -0200

    initial topic stuff

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   #sections controller
   get '/sections/:id' => 'sections#show'
 
+  #topics controller
+  get '/sections/topics/:id' => 'topics#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
