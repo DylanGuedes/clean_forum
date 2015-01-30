195bb45d4f8033228372e764234201c1f8fc6efa
DylanGuedes <djmgguedes@gmail.com>
2015-01-30 16:42:55 -0200

    stable users signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :posts, dependent: :delete_all
+
+  validates :login, length: { maximum:50, minimum: 5 },
+                          uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
   has_secure_password
 end
