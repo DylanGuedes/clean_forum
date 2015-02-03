fa1d4822f99e684eb2d740a60bb06f95507e31c4
DylanGuedes <djmgguedes@gmail.com>
2015-02-03 21:27:33 -0200

    refactoring for codeclimate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include SessionsHelper
+
   protect_from_forgery with: :exception
+
+  def generic_create type_class, father_class, father_id, type_params
+    father = father.class.find(params[:father_id])
+    type = father
+  end
+
+  def create_save type
+    if type.save
+      redirect_to type
+    else
+      flash[:notice] = "invalid #{type.class}. :("
+      render 'new'
+    end
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,12 +22,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.build(post_params)
     @post.user = current_user
-    if @post.save
-      redirect_to @post
-    else
-      flash[:notice] = "invalid post. :("
-      render 'new'
-    end
+    create_save @post
   end
 
   def show
@@ -45,5 +40,13 @@ class PostsController < ApplicationController
 
   def report_params
     params.require(:report_post).permit(:description, :user, :post, :type, :post_id)
+  end
+
+  def login_filter
+    unless signed_in?
+      store_location
+      flash[:error] = "You are not signed in!"
+      redirect_to signin_path
+    end
   end
 end
