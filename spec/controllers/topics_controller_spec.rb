a33fad62b65f9daa41427e18312086ef2802b0b8
DylanGuedes <djmgguedes@gmail.com>
2015-02-03 16:30:29 -0200

    specs for topics

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -6,13 +6,18 @@ RSpec.describe TopicsController, :type => :controller do
   before do
     @section = FactoryGirl.create(:section)
     @user = FactoryGirl.create(:user)
-    @topic = FactoryGirl.create(:topic)
+    
   end
 
+  let(:valid_attributes){ FactoryGirl.attributes_for :topic }
+  let(:invalid_attributes){ FactoryGirl.attributes_for :topic, title: '' }
+
   describe "GET" do
-    describe '#new' do
+    subject { @topic = FactoryGirl.create(:topic) }
+    describe '#new' do      
       context "logged user" do
         it "should render the new page with success" do
+          subject
           sign_in @user
           get :new, :id => @section.id
           expect(response).to have_http_status(:success)
@@ -20,6 +25,7 @@ RSpec.describe TopicsController, :type => :controller do
       end
       context "non-logged user" do
         it "should be redirected" do
+          subject
           get :new, :id => @section.id
           expect(response).to have_http_status(:redirect)
         end
@@ -27,6 +33,7 @@ RSpec.describe TopicsController, :type => :controller do
     end
     describe '#show' do
       it "should render with success an already created topic" do
+        subject 
         get :show, :id => @topic.id
         expect(response).to have_http_status(:success)
       end
@@ -34,6 +41,7 @@ RSpec.describe TopicsController, :type => :controller do
     describe '#render_report' do
       context "logged user" do
         it "should return success" do
+          subject 
           get :render_report, :id => @topic.id
         end
       end
@@ -43,16 +51,18 @@ RSpec.describe TopicsController, :type => :controller do
   describe "POST" do
     describe '#create' do
       context "with valid params" do
+        subject { post :create, topic: valid_attributes, :section_id => @section.id }
         it "should return success" do
           sign_in @user
-          post :create, :topic => { :title => @topic.title, :subtitle => @topic.subtitle, :section => @section, :content_for_posts => @topic.content_for_posts }, :section_id => @section.id, :content => @topic.content_for_posts
-          expect(response).to have_http_status(:success)
-
+          expect(subject).to have_http_status(:redirect)
         end
-
       end
-
+      # context "with invalid params" do
+      #   subject { post :create, topic: invalid_attributes, :section_id => @section.id } -> not passing tt
+      #   it 'should re-render #new' do
+      #     expect(subject).to have_http_status(:redirect)
+      #   end
+      # end
     end
-
   end
 end
