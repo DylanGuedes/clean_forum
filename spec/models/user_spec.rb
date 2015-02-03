7d34d73271fdd0a585d9081f66ac1cd6de15aa38
DylanGuedes <djmgguedes@gmail.com>
2015-02-03 17:13:15 -0200

    new attributes expected in user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe User, :type => :model do
     @user = FactoryGirl.create(:user)
   end
 
-  it { expect(@user).to respond_to(:name, :email, :login, :password, :password_confirmation) }
+  it {
+      #insert new attributes here
+      expect(@user).to respond_to(:name, :email, :login, :password, :password_confirmation, :posts,
+        :report_topics, :report_posts, :topics, :admin)
+     }
 
 end
