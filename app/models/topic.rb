290b65678d0263e3765f7fffd2791c201773af4c
DylanGuedes <djmgguedes@gmail.com>
2015-02-03 19:19:05 -0200

    more expects for topics spec

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,7 +5,8 @@ class Topic < ActiveRecord::Base
   has_many :report_topics
 
   validates :content, :presence => true, :length => { minimum: 3, maximum: 9999999 }
-  validates :title, :presence => true  
+  validates :title, :presence => true, :length => { minimum: 5, maximum: 50 }
+  validates :subtitle, :length => { maximum: 50 }
   validates :user_id, :presence => true
   validates :section_id, :presence => true
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Topic, :type => :model do
 
   it { 
     #insert new attributes here
-    expect(@topic).to respond_to(:content, :user_id, :section_id, :report_topics, :visible)
+    expect(@topic).to respond_to(:content, :user_id, :section_id, :report_topics, :visible, :title, :subtitle)
    }
 
   it { expect(@topic).to be_valid }
@@ -26,6 +26,63 @@ RSpec.describe Topic, :type => :model do
         before { @topic.content = 'a'*9999999+'a' }
         it 'should not be valid' do
           expect(@topic).not_to be_valid
+        end
+      end
+    end
+    describe 'user_id' do
+      before { @topic.user_id = '' }
+      it 'must not allow an invalid/blank user_id' do
+        expect(@topic).not_to be_valid
+      end
+    end
+    describe 'section_id' do
+      before { @topic.section_id = '' }
+      it 'must not allow an invalid/blank section_id' do
+        expect(@topic).not_to be_valid
+      end
+    end
+    describe 'visible' do
+      it 'must have default value true' do
+        expect(@topic.visible).to eq(true)
+      end 
+    end
+    describe 'title' do
+      describe 'with less than 5 chars' do
+        before { @topic.title = 'aaa' }
+        it 'should not be valid' do
+          expect(@topic).not_to be_valid
+        end
+      end
+      describe 'blank' do
+        before { @topic.title = '' }
+        it 'should not be valid' do
+          expect(@topic).not_to be_valid
+        end
+      end
+      describe 'with more than 50 chars' do
+        before { @topic.title = 'a'*51 }
+        it 'should not be valid' do
+          expect(@topic).not_to be_valid
+        end
+      end
+    end
+    describe 'subtitle' do
+      describe 'with less than 5 chars' do
+        before { @topic.subtitle = 'aaaa' }
+        it 'should be valid' do
+          expect(@topic).to be_valid
+        end
+      end
+      describe 'with more than 50 chars' do
+        before { @topic.subtitle = 'a'*51 }
+        it 'should not be valid' do
+          expect(@topic).not_to be_valid
+        end        
+      end
+      describe 'blank' do
+        before { @topic.subtitle = '' }
+        it 'should be valid' do
+          expect(@topic).to be_valid
         end
       end
     end
