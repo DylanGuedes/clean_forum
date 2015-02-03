5c30c1fd6444a057bd6d644b387aa1facdfbc35a
DylanGuedes <djmgguedes@gmail.com>
2015-02-02 23:19:11 -0200

    finished sectionscontrollerspec

--- spec/controllers/sections_controller_spec.rb
+++ spec/controllers/sections_controller_spec.rb
@@ -37,10 +37,15 @@ RSpec.describe SectionsController, :type => :controller do
   describe "POST #create" do
     context "with valid params" do
       it "should return the last section if the section was created" do
-        @user.admin = true
-        @user.save
+
         post :create, :section => { :name => 'Randomname', :description => 'Randomdescription' }
         expect(Section.last).to eq(Section.find_by(:name => 'Randomname'))
+      end
+    end
+    context "with invalid params" do
+      it "should render new" do
+        post :create, :section => { :name => '', :description => '' }
+        expect(response).to render_template(:new)
       end
     end
   end
